ce5e7ff96a8912cf75e36b8973f239d3d117129f
Max Reznichenko <max.reznichenko@gmail.com>
2015-05-23 18:57:07 +0300

    Another import of GPG keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,8 @@ cd /opt
 git clone https://Ser1al@github.com/Ser1aL/uawc-final
 cd uawc-final
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
 curl -L https://get.rvm.io | bash -s stable
 source /usr/local/rvm/scripts/rvm
 echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
